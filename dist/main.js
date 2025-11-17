95c5f1ac7ed7e678e15d56aec74751649bbeb8ec
shukina7olga <olga.shchukina.99@mail.ru>
2025-11-18 02:22:01 +0300

    db json-server firebase

--- dist/main.js
+++ dist/main.js
@@ -30,13 +30,33 @@ eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpa
 
 /***/ }),
 
+/***/ "./src/modules/getData.js":
+/*!********************************!*\
+  !*** ./src/modules/getData.js ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst getData = () => {\r\n    return fetch('https://newtest-d30d8-default-rtdb.firebaseio.com/goods.json')\r\n        .then((response) => {\r\n            return response.json();\r\n        })\r\n\r\n}\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (getData); \n\n//# sourceURL=webpack://db/./src/modules/getData.js?\n}");
+
+/***/ }),
+
+/***/ "./src/modules/postData.js":
+/*!*********************************!*\
+  !*** ./src/modules/postData.js ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst postData = () => {\r\n    return fetch('http://localhost:3000/goods/94f4', {\r\n        method: 'DELETE',\r\n      })\r\n      .then(res => res.json())\r\n\r\n    // return fetch('http://localhost:3000/goods', {\r\n    //     method: 'POST',\r\n    //     body: JSON.stringify({\r\n    //         title: \"Игра kitika\",\r\n    //         price: 7770,\r\n    //         sale: true,\r\n    //         img: \"https://cdn1.ozone.ru/multimedia/c400/1023547851.jpg\",\r\n    //         category: \"Игры и софт\"\r\n    //       }),\r\n    //     headers: {\r\n    //       'Content-type': 'application/json; charset=UTF-8',\r\n    //     },\r\n    //   })\r\n    //   .then(res => res.json())\r\n\r\n}\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (postData); \n\n//# sourceURL=webpack://db/./src/modules/postData.js?\n}");
+
+/***/ }),
+
 /***/ "./src/modules/second.js":
 /*!*******************************!*\
   !*** ./src/modules/second.js ***!
   \*******************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst second = () => {\r\n    const cartBtn = document.getElementById('cart')\r\n    console.log(\"cartBtn\")\r\n}\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (second);\n\n//# sourceURL=webpack://db/./src/modules/second.js?\n}");
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _getData__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./getData */ \"./src/modules/getData.js\");\n/* harmony import */ var _postData__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./postData */ \"./src/modules/postData.js\");\n\r\n\r\n\r\nconst second = () => {\r\n    const cartBtn = document.getElementById('cart')\r\n\r\n    ;(0,_getData__WEBPACK_IMPORTED_MODULE_0__[\"default\"])().then((data) => {\r\n        console.log(data);\r\n    })\r\n\r\n\r\n    // cartBtn.addEventListener('click', () => {\r\n    //     postData().then((data) => {\r\n    //         console.log(data);\r\n    //         getData().then((data) => {\r\n    //             console.log(data);\r\n    //         })\r\n    //     })\r\n    // })\r\n}\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (second);\n\n//# sourceURL=webpack://db/./src/modules/second.js?\n}");
 
 /***/ })
 
